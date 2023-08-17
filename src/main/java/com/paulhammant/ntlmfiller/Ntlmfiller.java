402d32e273266d8d1090805d86236a47aa127688
Paul Hammant <paul@hammant.org>
2023-08-17 09:30:06 +0100

    parameterize

--- src/main/java/com/paulhammant/ntlmfiller/Ntlmfiller.java
+++ src/main/java/com/paulhammant/ntlmfiller/Ntlmfiller.java
@@ -6,17 +6,24 @@ import org.sikuli.script.Location;
 import org.sikuli.script.Screen;
 
 public class Ntlmfiller {
-    public static void main(String[] args) throws FindFailed {
+
+    public Ntlmfiller() {
+    }
+
+    public static void fillNtlmAndSubmit(String processName, String username, String password) throws FindFailed {
         App.logOn();
-        App.focus("chrome");
+        App.focus(processName);
         Screen screen = new Screen();
         int y = (screen.h / 2 * -1) - 300;
         System.out.println(screen.h + " " + y);
 
         //screen.offset(0, y).click();
         screen.hover(new Location(1308, 41));
-        screen.type("usernameeee\t");
-        screen.type("passwoooooord\t\t");
+        screen.type(username + "\t");
+        screen.type(password + "\t\t");
         screen.type(" "); // click OK button
+    }
+    public static void main(String[] args) throws FindFailed {
+        fillNtlmAndSubmit("chrome", "usernameeeee", "passwordddd");
     }
 }
